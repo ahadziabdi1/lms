2b92b570d47e1a18cd0ce9c5b2c70493c44bace7
ahadziabdi1 <ahadziabdi1@etf.unsa.ba>
2025-03-10 21:56:28 +0100

    implement footer component and enhance course details and courses list pages with filtering functionality

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AddContext } from "../../context/AddContext";
+import Loading from "../../components/student/Loading";
 
 const CourseDetails = () => {
-  return (
-    <div>
-      <h1>CourseDetails</h1>
-    </div>
+  const { id } = useParams();
+  const [courseData, setCourseData] = useState(null);
+  const { allCourses } = useContext(AddContext);
+
+  const fetchCourseData = async () => {
+    const findCourse = allCourses.find((course) => course._id === id);
+    setCourseData(findCourse);
+  };
+
+  useEffect(() => {
+    fetchCourseData();
+  }, []);
+
+  return courseData ? (
+    <>
+      <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left">
+        <div className="absolute top-0 left-0 w-full h-[500px] z-[-10] bg-gradient-to-b from-cyan-100 to-white"></div>
+        {/*left column*/}
+        <div>
+          <h1>{courseData.courseTitle}</h1>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: courseData.courseDescription.slice(0, 200),
+            }}
+          ></p>
+        </div>
+        {/*right column*/}
+        <div></div>
+      </div>
+    </>
+  ) : (
+    <Loading />
   );
 };
 
